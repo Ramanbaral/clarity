9461d89bb1c1f0a83c2a080c99410430c99f700f
raman baral <ramanisstudent@gmail.com>
2026-02-10 21:43:36 +0545

    setup react client

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="font-bold text-blue-500 grid place-content-center h-screen">Clarity</p>
+    </>
+  );
+}
+
+export default App;
