6bc1d9905dbb1f01a1e0e1bb33ec6181e879661b
raman baral <ramanisstudent@gmail.com>
2026-02-10 22:46:49 +0545

    chore: setup swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Clarity API')
+    .setDescription('Personal Expense Tracker API documentation')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('transactions')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
